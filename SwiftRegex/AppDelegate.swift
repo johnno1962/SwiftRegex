1a4eedb8108ed65e9292d659cdb968dc12725639
John Holdsworth <github@johnholdsworth.com>
2015-04-20 20:03:42 +0100

    Back out PR #2

--- SwiftRegex/AppDelegate.swift
+++ SwiftRegex/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SwiftRegex
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- SwiftRegex/ViewController.swift
+++ SwiftRegex/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SwiftRegex
 
 class ViewController: UIViewController, UITextViewDelegate {
                             
